359e0084563d135f9480a01b4b2c6b1f9959ba54
Mahendra Patel <Mpatel6695@conestogac.on.ca>
2024-02-04 11:57:00 -0500

    Added code to user choice from given list from 1, 2, or 3.

--- Virtual_Pet_Simulator/Program.cs
+++ Virtual_Pet_Simulator/Program.cs
@@ -6,8 +6,10 @@ public static class Program
     public static void Main()
     {
         Console.WriteLine("Please Choose a type pet:");
-		Console.WriteLine("1. Cat");
+        Console.WriteLine("1. Cat");
         Console.WriteLine("2. Dog");
         Console.WriteLine("3. Rabbit");
+        Console.WriteLine("Enter your choice from given list from 1, 2, or 3. ");
+        Console.Write("User InPut : ");
     }
 }
