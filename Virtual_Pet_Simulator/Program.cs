0187846e1ba9cbdea05b27347d44a6b26dad28dc
Mahendra Patel <Mpatel6695@conestogac.on.ca>
2024-02-04 11:50:41 -0500

    Added different  Catagory  of Pets

--- Virtual_Pet_Simulator/Program.cs
+++ Virtual_Pet_Simulator/Program.cs
@@ -6,5 +6,8 @@ public static class Program
     public static void Main()
     {
         Console.WriteLine("Please Choose a type pet:");
+		Console.WriteLine("1. Cat");
+        Console.WriteLine("2. Dog");
+        Console.WriteLine("3. Rabbit");
     }
 }
