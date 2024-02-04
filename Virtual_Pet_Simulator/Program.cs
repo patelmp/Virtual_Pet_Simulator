ad243e943abf909ec53432918f6901fc49d7f1bf
mpate <mpate@ARULUV>
2024-02-04 10:39:14 -0500

    Add project files.

---
+++ Virtual_Pet_Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public static class Program
+{
+    private static readonly string name;
+
+    public static void Main()
+    {
+        Console.WriteLine("Please Choose a type pet:");
+    }
+}
